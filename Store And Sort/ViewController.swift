5a774485c78fc3df851b7c27b189fb36113cd3db
paul <paul@sfs-wifi-dhcp-10-143-147-60.wifi.sfsu.edu>
2018-11-27 13:30:00 -0800

    create a new project, add gitignore and git attributes

---
+++ Store And Sort/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Store And Sort
+//
+//  Created by paul on 11/27/18.
+//  Copyright © 2018 PoHung Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
